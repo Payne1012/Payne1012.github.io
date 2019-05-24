3b25e36334ee33964c3a72f78664e55c79b10f15
payne1012 <1339155668@qq.com>
2019-05-24 13:18:29 +0800

    angular项目增加本地数据请求示例

--- item/angular/admin/news/ctrl.js
+++ item/angular/admin/news/ctrl.js
@@ -4,7 +4,7 @@
 app.controller('ListController', function($scope, $resource,$stateParams,$modal,$state) {
     //查询
     $scope.query = function(page,filter){
-        var $com = $resource($scope.app.host + "data/cart.json");
+        var $com = $resource($scope.app.host + "data/news.json");
         if(!page){
             page=1;
         }else{
@@ -13,17 +13,17 @@ app.controller('ListController', function($scope, $resource,$stateParams,$modal,
         $com.get({page:page,filter:filter},function(data){
 //      	
             //扩展分页数据，显示页签，最终效果为  < 1 2 3 4 5 >
-            data.page_index = page;
+            data.length = page;
             data.pages = [];    //页签表
             var N = 5;          //每次显示5个页签
             var s = Math.floor(page/N)*N;
             if(s==page)s-=N;
             s += 1;
-            var e = Math.min(data.page_count,s+N-1)
+            var e = Math.min(data.length,s+N-1)
             for(var i=s;i<=e;i++)
                 data.pages.push(i)
             $scope.data = data.result.list;
-            console.log($scope.data)
+            console.log(e)
             $scope.search_context = filter;
         });
     }
@@ -35,7 +35,7 @@ app.controller('ListController', function($scope, $resource,$stateParams,$modal,
     var selected = false;
     $scope.selectAll = function(){
         selected = !selected;
-        angular.forEach($scope.data.results,function(item){
+        angular.forEach($scope.data,function(item){
             item.selected = selected;
         });
     }
@@ -43,7 +43,7 @@ app.controller('ListController', function($scope, $resource,$stateParams,$modal,
     $scope.exec = function(){
         if($scope.operate=="1"){
             var ids = [];
-            angular.forEach($scope.data.results,function(item){
+            angular.forEach($scope.data,function(item){
                 if(item.selected){
                     ids.push(item.id);
                 }
